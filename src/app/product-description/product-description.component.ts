2661025cd050e13019154a8c15948d8fefa869db
slowwwburn <olalekanbalogun@ymail.com>
2019-11-07 16:03:12 +0100

    Completed module 2

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -12,7 +12,7 @@ export class ProductDescriptionComponent implements OnInit {
   constructor(private _productService:ProductService) { }
 
   ngOnInit() {
-    this._productService.geteAlbum(1).subscribe(response => this.albumInfo = response)
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
   }
 
 }
